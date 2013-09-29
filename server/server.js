b98b12ce2ca2f4d920fff5f99c4d115fecd2a64c
ubuntu <ubuntu@i-9ef4643c.greenqloud.internal>
2013-09-29 17:03:07 +0000

    Latest changes.

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 HashStore = new Meteor.Collection("hashstore");
+LastMatchStore = new Meteor.Collection("lastmatchstore");
 
 if (Meteor.isClient) {
   Template.hello.greeting = function () {
@@ -21,14 +22,30 @@ if (Meteor.isServer) {
 
   Meteor.Router.add({
 
-  '/match/:id': function(id) {
+  '/compare/:id': function(id) {
         
-        p = id.split("-");
+        var p = id.split("-");
         var JSONObj = {"phone":p[0], "hash":p[1]};
         var valInStore = HashStore.findOne(JSONObj);
         console.log(JSONObj);
-        if(valInStore == null) return "NO";
-        else return "YES";
+        if(valInStore == null) 
+        {
+	  JSONObj = {};
+	  JSONObj[p[0]] = "NO";
+         // JSONObj = {p[0] :"NO"};
+          LastMatchStore.insert(JSONObj);
+          debugString = JSON.stringify(JSONObj);
+          return debugString;
+        }
+        else 
+        {
+	  JSONObj = {};
+	  JSONObj[p[0]] = "YES";
+          //JSONObj = {p[0] :"YES"};
+          LastMatchStore.insert(JSONObj);
+          debugString = JSON.stringify(JSONObj);
+          return debugString;
+        }
       }
   });
 
@@ -39,6 +56,27 @@ if (Meteor.isServer) {
         HashStore.insert(JSONObj);
         debugString = JSON.stringify(JSONObj);
         return debugString;
+      }
+
+  });
+
+  Meteor.Router.add({
+  '/lastdidmatch/:id': function(id) {
+	JSONObj = {};
+	JSONObj[id] = "YES";
+        //var JSONObj = {id:"YES"};
+        var valInStore = LastMatchStore.findOne(JSONObj);
+        if(valInStore == null)
+        {
+          var JSONObj = {"lastmatched":0};
+          return JSON.stringify(JSONObj);
+        }
+        else
+        {
+          var JSONObj = {"lastmatched":1};
+          return JSON.stringify(JSONObj);
+        }
+
       }
 
   });
