7f786034277aaaefa8a29e1a7919e3148e74b14b
ubuntu <ubuntu@i-9ef4643c.greenqloud.internal>
2013-09-29 03:13:39 +0000

    iLatest version - awesome!

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+HashStore = new Meteor.Collection("hashstore");
+
 if (Meteor.isClient) {
   Template.hello.greeting = function () {
     return "Pass a gesture hash as a REST argument h=";
@@ -19,8 +21,24 @@ if (Meteor.isServer) {
 
   Meteor.Router.add({
 
-  '/h/:id': function(id) {
-        return id;
+  '/match/:id': function(id) {
+        
+        p = id.split("-");
+        var JSONObj = {"phone":p[0], "hash":p[1]};
+        var valInStore = HashStore.findOne(JSONObj);
+        console.log(JSONObj);
+        if(valInStore == null) return "NO";
+        else return "YES";
+      }
+  });
+
+  Meteor.Router.add({
+  '/set/:id': function(id) {
+        p = id.split("-");
+        var JSONObj = {"phone":p[0], "hash":p[1]};
+        HashStore.insert(JSONObj);
+        debugString = JSON.stringify(JSONObj);
+        return debugString;
       }
 
   });
